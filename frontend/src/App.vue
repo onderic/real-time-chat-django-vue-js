830d722bf6d7b1f652e4e42194bb1cca78135249
onderi <ondericollins80@gmail.com>
2023-06-13 12:44:05 +0300

    setup frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template>
+  <header>
+    <div>
+      <h1 class="text-green-600 text-5xl">hello vue</h1>
+    </div>
+   
+  </header>
+
+  <RouterView />
+</template>
+
+<style>
+</style>
