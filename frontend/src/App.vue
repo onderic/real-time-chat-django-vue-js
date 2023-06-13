ae4ce86dafd2a2f559a4345a598e005a08b18ef1
onderi <ondericollins80@gmail.com>
2023-06-13 13:35:33 +0300

    setuppage layout

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,14 +4,40 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header>
-    <div>
-      <h1 class="text-green-600 text-5xl">hello vue</h1>
+<div class="flex  bg-white  h-screen">
+  <div class="w-1/4 h-screen  bg-gray-100">
+    <div class="bg-gray-100 py-10 px-4   w-full border-b border-gray-200 flex items-center justify-between fixed top-0">
+      
     </div>
+    <!-- Sidebar content -->
+    
+  </div>
+  <div class="">
+    <nav class=" bg-gray-100 py-6 px-4  flex items-center justify-between fixed top-0 left-1/4 w-3/4  border-l border-gray-200">
+      <div class="flex items-center">
+        <img src="logo.png" alt="Logo" class="w-8 h-8 mr-2">
+        <span class="text-gray-800 font-bold text-lg">Chat</span>
+      </div>
+      <ul class="flex items-center space-x-4">
+      
+        <li><a href="#" class="text-gray-800">Services</a></li>
+        <li><a href="#" class="text-gray-800">Contact</a></li>
+      </ul>
+    </nav>
    
-  </header>
+  </div>
+  <div class=" mt-20 px-8 text-white py-8 w-3/4">
+      <!-- Main content -->
+      <h1 class="text-2xl font-bold">Welcome to the Chat App!</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam malesuada erat ut nunc consectetur, ut finibus mi eleifend. Suspendisse efficitur ligula id volutpat sagittis. Duis sed interdum turpis.</p>
+    </div>
+  <div>
+    
+  </div>
+</div>
 
-  <RouterView />
+ 
+  <!-- <RouterView /> -->
 </template>
 
 <style>
