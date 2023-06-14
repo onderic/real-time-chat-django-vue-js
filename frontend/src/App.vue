b9fb6b927a41b354c8064d1b2900120094309708
onderi <ondericollins80@gmail.com>
2023-06-14 19:28:45 +0300

    latest

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,49 +2,49 @@
   <div class="tracking-wider  flex flex-col min-h-screen">
     <div class="relative flex min-h-screen" v-if="userStore.user.isAuthenticated">
       <!-- Sidebar content -->
-      <div class="bg-slate-300  dark:text-white w-1/3 space-x-6 shadow-lg border border-gray-300 px-0 py-4 absolute inset-y-0 left-0 md:relative md:-translate-x-0 transform -translate-x-full transition duration-200 ease-in-out">
-        <!-- Sidebar content -->
-        <div class="bg-slate-400 py-2 px-4 overflow-y-auto flex items-center justify-between fixed top-0 w-full border border-b-0">
-          <div class="flex items-center">
-            <img :src="userStore.user.user_avatar" alt="logo" class="mr-2 h-12 w-12 rounded-full border-2 border-green-400">
+     
+        <div class="bg-slate-300 dark:text-white w-1/3 space-x-6  fixed top-0 shadow-lg border border-gray-300 px-0 py-4  inset-y-0 left-0 md:relative md:-translate-x-0 transform -translate-x-full transition duration-200 ease-in-out">
+          <!-- Sidebar content -->
+          <div class="bg-slate-400 py-2 px-4 overflow-y-auto flex items-center justify-between fixed top-0 w-full border border-b-0">
+            <div class="flex items-center">
+              <img :src="userStore.user.user_avatar" alt="logo" class="mr-2 h-12 w-12 rounded-full border-2 border-green-400">
+            </div>
+            <div class="ml-auto">
+              <button class="bg-sky-500 hover:bg-sky-700 text-white font-bold py-1 px-2 rounded-md" @click="logout()">logout</button>
+            </div>
           </div>
-          <div class="ml-auto">
-            <button class="bg-sky-500 hover:bg-sky-700 text-white font-bold py-1 px-2 rounded-md" @click="logout()">logout</button>
-          </div>
-        </div>
 
+            <div class="mt-20">
+              <div class="mr-4">
+                <input
+                  type="text"
+                  v-model="searchTerm"
+                  @input="filterItems"
+                  placeholder="Search..."
+                  class="border border-gray-100 bg-white rounded-md px-4 py-2 w-full pl-10"
+                />
+                <ul v-if="filteredItems.length > 0" class="mt-4">
+                  <li v-for="(item, index) in filteredItems" :key="index" class="border rounded-md mr-2 border-gray-50 mb-3 px-4 py-2">
+                    <div
+                      class="flex items-center space-x-1 w-full px-4 py-2 rounded-md cursor-pointer"
+                      :class="{ 'bg-white': selectedUser === item.username }"
+                      @click="selectUser(item.username)"
+                    >
+                      <img
+                        :src="item.user_user_avatar"
+                        alt=""
+                        class="mr-2 h-8 w-8 bg-white p-2 rounded-full"
+                      />
+                      <span class="mb-2" :class="{ 'font-bold': selectedUser === item.username }">{{ item.username }}</span>
+                    </div>
+                  </li>
+                </ul>
+                <p v-else-if="searchTerm" class="mt-2">No results found.</p>
+                <p v-else class="mt-2">Type to search.</p>
+              </div>
+            </div>
+          </div>
 
-        <div class="mt-20">
-          <div class="mr-4">
-            
-            <input
-              type="text"
-              v-model="searchTerm"
-              @input="filterItems"
-              placeholder="Search..."
-              class="border border-gray-100 bg-white rounded-md px-4 py-2 w-full pl-10"
-            />
-            <ul v-if="filteredItems.length > 0" class="mt-4">
-          <li v-for="(item, index) in filteredItems" :key="index" class="border rounded-md mr-2 border-gray-50 mb-3 px-4 py-2">
-          <div
-            class="flex items-center space-x-1 w-full px-4 py-2 rounded-md cursor-pointer"
-            :class="{ 'bg-white': selectedUser === item.username }"
-            @click="selectUser(item.username)"
-          >
-            <img
-              :src="item.user_user_avatar"
-              alt=""
-              class="mr-2 h-8 w-8 bg-white p-2 rounded-full"
-            />
-            <span class="mb-2" :class="{ 'font-bold': selectedUser === item.username }">{{ item.username }}</span>
-          </div>
-        </li>
-      </ul>
-            <p v-else-if="searchTerm" class="mt-2">No results found.</p>
-            <p v-else class="mt-2">Type to search.</p>
-          </div>
-        </div>
-      </div>
 
       <!-- Main content -->
           <div class="h-screen w-full overflow-y-auto">
@@ -59,15 +59,16 @@
               </nav>
             </div>
 
-            <div :class="{ 'bg-gray-100': selectedUser, 'bg-white': !selectedUser }" class="flex flex-col min-h-screen">
+            <div :class="{ 'bg-gray-50': selectedUser, 'bg-slate-300': !selectedUser }" class="flex flex-col min-h-screen">
             <div class="px-4 mt-20 py-8">
-              <div class="w-1/4">
-                <h2 v-if="selectedUser" class="bg-white px-4 py-4">Selected User: {{ selectedUser }}</h2>
+              <div class="w-1/2 ">
+                <h2 v-if="selectedUser" class="bg-white ml-5 rounded-md shadow-md px-4 py-4">Selected User: {{ selectedUser }}</h2>
               </div>
             </div>
-            <div class="w-1/4 relative flex items-center">
-                <h2 v-if="selectedUser" class="bg-green-400 px-4 py-4">Selected User: {{ selectedUser }}</h2>
-              </div>
+            <div class="w-1/2 ml-auto">
+              <h2 v-if="selectedUser" class="bg-green-200 mr-5 rounded-md shadow-md px-4 py-4">Selected User: {{ selectedUser }}</h2>
+            </div>
+
             <div class="flex-grow"></div> <!-- Add this div to push the footer to the bottom -->
 
             <div v-if="selectedUser" class="mb-3 bg-slate-400 p-4">
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -8,6 +8,7 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import axios from 'axios'
+import Toast from './components/Toast.vue'
 
 
 axios.defaults.baseURL = 'http://127.0.0.1:8000/'
@@ -20,5 +21,6 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router, axios)
 
+app.component('toast', Toast )
 
 app.mount('#app')
